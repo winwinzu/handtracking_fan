29af4153edb90d5215d68528a197a49c399c99b1
winwinzu <ftakuabai5000@gmail.com>
2021-07-12 20:12:06 +0900

    Before push

--- Arduino/HandTracking_Fan/src/main.cpp
+++ Arduino/HandTracking_Fan/src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
+#include <string.h>
 
 void setup() {
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
   // put your setup code here, to run once:
 }
 
 void loop() {
+  // digitalWrite(2, HIGH);
+  if(Serial.available())
+  {
+    String s = Serial.readStringUntil(':');
+    // int str = Serial.read();
+    if(s.equals("aa"))
+    {
+      digitalWrite(2, HIGH);
+    }
+    analogWrite(3, s.toInt());
+  }
   // put your main code here, to run repeatedly:
 }
